a7b8e0f8f6eeeff289f099daf5d4cc054d34c363
harry7naga <h4ry7naga@gmail.com>
2020-09-10 06:45:29 +0700

    Update Tugas 13

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import './App.css';
 // import Tugas10 from './Tugas-10/Tugas10'
 // import LifeCycle from './Tugas-11/LifeCycle';
 // import Coba from './Tugas-12/coba';
-import Tugas12 from './Tugas-12/Tugas12';
+// import Tugas12 from './Tugas-12/Tugas12';
+import Tugas13 from './Tugas-13/Tugas13';
+import daftarBuah from './Tugas-13/daftarBuah';
 
 
 
 function App() {
   return (
     <div>
-      <Tugas12 />
+      <Tugas13 />
     </div>
   );
 }
